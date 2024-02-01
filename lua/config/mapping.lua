29ae8dd41f72f081a5f14f92c78ba57c15c5dd14
Damir Akhmadullin <damir@damir.local>
2024-02-01 20:37:59 +0300

    feat: update lock and mappings

--- lua/config/mapping.lua
+++ lua/config/mapping.lua
@@ -4,6 +4,9 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 vim.keymap.set('n', '<leader>/', ':CommentToggle<CR>')
+vim.keymap.set('n', '<leader>q', ':q<cr>')
+vim.keymap.set('n', '<S-l>', ':bnext<cr>')
+vim.keymap.set('n', '<S-h>', ':bprev<cr>')
 
 
 -- Russian nvim layout
