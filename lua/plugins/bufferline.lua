d73b52c1fa97d1ee7d7d2b1c9ecad485e6f0b515
Артур Давидов <arturdavidov@Arturs-MacBook-Air.local>
2024-02-01 18:58:10 +0300

    bufferline plugin

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,26 @@
+return {
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("bufferline").setup({
+			options = {
+				buffer_close_icon = "",
+				mode = "buffers",
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "File Explorer",
+						separator = true,
+						padding = 1,
+					},
+				},
+				diagnostics = "nvim_lsp",
+				indicator = {
+					icon = "  ",
+					style = "icon",
+				},
+				separator_style = "slope",
+			},
+		})
+	end,
+}
