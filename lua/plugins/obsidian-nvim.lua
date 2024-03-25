8a27bdb93f09ecd0f1c8893b5ec0af1a10cdc067
Артур Давидов <arturdavidov@Arturs-MacBook-Air.local>
2024-03-25 21:21:46 +0300

    obsidian nvim udate

--- lua/plugins/obsidian-nvim.lua
+++ lua/plugins/obsidian-nvim.lua
@@ -3,68 +3,74 @@ local vaultPath = user_settings.env.vaultPath
 local user = user_settings.env.user
 
 return {
-    "epwalsh/obsidian.nvim",
-    version = "*", -- recommended, use latest release instead of latest commit
-    lazy = true,
-    ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-    --   "BufReadPre path/to/my-vault/**.md",
-    --   "BufNewFile path/to/my-vault/**.md",
-    -- },
-    dependencies = {
-        -- Required.
-        "nvim-lua/plenary.nvim",
-        "hrsh7th/nvim-cmp", -- for [[]] completion
+	"epwalsh/obsidian.nvim",
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = true,
+	ft = "markdown",
+	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+	-- event = {
+	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+	--   "BufReadPre path/to/my-vault/**.md",
+	--   "BufNewFile path/to/my-vault/**.md",
+	-- },
+	dependencies = {
+		-- Required.
+		"nvim-lua/plenary.nvim",
+		"hrsh7th/nvim-cmp", -- for [[]] completion
 
-        -- see below for full list of optional dependencies 👇
-    },
-    opts = {
-        workspaces = {
-            {
-                name = user,
-                path = vaultPath,
-            },
-            -- {
-            --   name = "work",
-            --   path = "~/vaults/work",
-            -- },
-            -- can have multiple workplaces
-        },
-        mappings = {
-            -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-            ["gd"] = {
-                action = function()
-                    return require("obsidian").util.gf_passthrough()
-                end,
-                opts = { noremap = false, expr = true, buffer = true },
-            },
-            -- Toggle check-boxes.
-            ["<leader>ch"] = {
-                action = function()
-                    return require("obsidian").util.toggle_checkbox()
-                end,
-                opts = { buffer = true },
-            },
-        },
-        open_app_foreground = true,
-        note_id_func = function(title)
-            local suffix = ""
-            if title ~= nil then
-                suffix = title:gsub(" ", "-"):gsub("[^А-ЯA-Za-z0-9%-]", ""):lower()
-            else
-                for _ = 1, 4 do
-                    suffix = suffix .. string.char(math.random(65, 90))
-                end
-            end
-            return tostring(os.time()) .. "-" .. suffix
-        end,
+		-- see below for full list of optional dependencies 👇
+	},
+	opts = {
+		workspaces = {
+			{
+				name = user,
+				path = vaultPath,
+			},
+			-- {
+			--   name = "work",
+			--   path = "~/vaults/work",
+			-- },
+			-- can have multiple workplaces
+		},
+		mappings = {
+			-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+			["gd"] = {
+				action = function()
+					return require("obsidian").util.gf_passthrough()
+				end,
+				opts = { noremap = false, expr = true, buffer = true },
+			},
+			-- Toggle check-boxes.
+			["<leader>ch"] = {
+				action = function()
+					return require("obsidian").util.toggle_checkbox()
+				end,
+				opts = { buffer = true },
+			},
+		},
+		templates = {
+			subdir = "templates",
+			date_format = "%Y-%m-%d",
+			time_format = "%H:%M",
+			substitutions = {},
+		},
+		open_app_foreground = true,
+		note_id_func = function(title)
+			local suffix = ""
+			if title ~= nil then
+				suffix = title:gsub(" ", "-"):gsub("[^А-ЯA-Za-яa-z0-9%-]", ""):lower()
+			else
+				for _ = 1, 4 do
+					suffix = suffix .. string.char(math.random(65, 90))
+				end
+			end
+			return tostring(os.time()) .. "-" .. suffix
+		end,
 
-        note_path_func = function(spec)
-            local path = spec.dir / tostring(spec.id)
-            return path:with_suffix(".md")
-        end,
-    },
+		note_path_func = function(spec)
+			local path = spec.dir / tostring(spec.id)
+			return path:with_suffix(".md")
+		end,
+	},
 }
