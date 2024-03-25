0800a17d789cbcfcd81d1ff195ad6f1320d50b5c
Артур Давидов <arturdavidov@Arturs-MacBook-Air.local>
2024-03-25 19:42:16 +0300

    obsidian nvim update 2

--- lua/plugins/obsidian-nvim.lua
+++ lua/plugins/obsidian-nvim.lua
@@ -53,7 +53,7 @@ return {
         note_id_func = function(title)
             local suffix = ""
             if title ~= nil then
-                suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+                suffix = title:gsub(" ", "-"):gsub("[^А-ЯA-Za-z0-9%-]", ""):lower()
             else
                 for _ = 1, 4 do
                     suffix = suffix .. string.char(math.random(65, 90))
